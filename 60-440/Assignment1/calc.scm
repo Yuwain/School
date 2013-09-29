f1f5e439e1a1dc071673eac9e55c130d674f2c74
Jeremy High <Jeremy.M.High@gmail.com>
2013-09-29 15:02:22 -0400

    Added 60-440 files

---
+++ 60-440/Assignment1/calc.scm
@@ -0,0 +1,18 @@
+|# Written by: Jeremy High
+   Student ID: 103306452
+   Written on: September 20th, 2013 #|
+
+;A left-associative calculator for addition and subtraction
+(define (calculator expr)
+  (cond ((number? expr)
+         expr)
+        ((eq? (length expr) 1)
+         (car expr))
+        (else
+         (let ((head ((lambda (expr)
+                        (let ((op (eval (cadr expr)))
+                              (num1 (calculator (car expr)))
+                              (num2 (calculator (caddr expr))))
+                          (op num1 num2))) expr))
+               (tail (cdddr expr)))
+           (calculator (cons head tail))))))
